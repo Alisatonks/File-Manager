77f124584733e772d8743f1c20f38360564c08f8
Volha Haiduk <Alisatonks2385@gmail.com>
2024-10-13 21:44:05 +0200

    feat: cpus, homedir, username

--- src/os/catalog.js
+++ src/os/catalog.js
@@ -1,7 +1,13 @@
 import getEOL from "./eol.js";
+import getCPUs from "./cpus.js";
+import getHomeDir from "./homedir.js";
+import getUserName from "./username.js";
 
 const catalog = {
-  "--EOL": getEOL
+  "--EOL": getEOL,
+  "--cpus": getCPUs,
+  "--homedir": getHomeDir,
+  "--username": getUserName,
 }
 
 export default catalog;

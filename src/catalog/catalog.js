0e134268a9177189bb127e218b8b22e473a5d696
Volha Haiduk <Alisatonks2385@gmail.com>
2024-10-13 13:50:53 +0200

    feat: create empty file

--- src/catalog/catalog.js
+++ src/catalog/catalog.js
@@ -2,12 +2,14 @@ import goUp from "../navigation/up.js";
 import navigate from "../navigation/navigate.js";
 import printContent from "../navigation/printContent.js";
 import readAndPrint from "../filesOperations/readAndPrint.js";
+import createFile from "../filesOperations/createFile.js";
 
 const catalog = {
     up: goUp,
     cd: navigate,
     ls: printContent,
     cat: readAndPrint,
+    add: createFile,
 }
 
 export default catalog;

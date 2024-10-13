792fa08e5296189503a94835a11ae8854655b1c8
Volha Haiduk <Alisatonks2385@gmail.com>
2024-10-13 21:47:41 +0200

    feat: add getarchitecture

--- src/os/catalog.js
+++ src/os/catalog.js
@@ -2,12 +2,14 @@ import getEOL from "./eol.js";
 import getCPUs from "./cpus.js";
 import getHomeDir from "./homedir.js";
 import getUserName from "./username.js";
+import getArchitecture from "./architecture.js";
 
 const catalog = {
   "--EOL": getEOL,
   "--cpus": getCPUs,
   "--homedir": getHomeDir,
   "--username": getUserName,
+  "--architecture": getArchitecture,
 }
 
 export default catalog;
